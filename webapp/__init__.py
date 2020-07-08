380ab7f94be46f4d864dab26cb329726a8b79da7
SimonSkade <simon.skade@gmx.net>
2020-07-08 16:27:35 +0200

    added user authentication

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,8 +1,28 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+import os
 
 
-app = Flask(__name__)
+app = Flask(__name__)#the heart of the app (or actually the whole app)
 
+#configuration
+#distinguish between server deploy (ENV=production) and local testing
+if os.environ.get("ENV") == "production":
+	app.config["DEBUG"] = False
+	app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")#get secret key from env
+	app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")#get database from environment
+else:
+	app.config["DEBUG"] = True
+	app.config["SECRET_KEY"] = "7g2fab9567unr8jvndush9zjn2hbnoug5ut"#just set this secret key
+	app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"#use or deploy a local database
+
+db = SQLAlchemy(app)#this is the database
+bcrypt = Bcrypt(app)#stuff for password encryption
+login_manager = LoginManager(app)
+login_manager.login_view = "login"
+login_manager.login_message_category = "info"
 
 
 from webapp import routes
