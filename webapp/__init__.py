57630bdde3d51acce97a8ac2c3e8bd2e5d7eeb61
LuanAdemi <luanademi@mail.de>
2020-07-14 17:43:27 +0200

    Added a new design for the webapp

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -17,6 +17,7 @@ else:
 	app.config["DEBUG"] = True
 	app.config["SECRET_KEY"] = "7g2fab9567unr8jvndush9zjn2hbnoug5ut"#just set this secret key
 	app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"#use or deploy a local database
+	app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0 # set the default max file age to 0
 
 db = SQLAlchemy(app)#this is the database
 bcrypt = Bcrypt(app)#stuff for password encryption
