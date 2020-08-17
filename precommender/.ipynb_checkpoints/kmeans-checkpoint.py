2243e2e836130cba99a6b8c7f01e27a08b6d3473
LuanAdemi <luanademi@mail.de>
2020-08-17 21:37:41 +0200

    Implemented KMeans++ which shows a way better performance

--- precommender/.ipynb_checkpoints/kmeans-checkpoint.py
+++ precommender/.ipynb_checkpoints/kmeans-checkpoint.py
@@ -2,8 +2,9 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import silhouette_score
 import numpy as np
 import random
+import sys
 
-#   An implementation of the kmeans algorithm with 
+#   An implementation of the kmeans++ (https://theory.stanford.edu/~sergei/papers/kMeansPP-soda.pdf) algorithm with 
 #   k optimization using the elbow method.
 
 class kmeans:
@@ -12,20 +13,48 @@ class kmeans:
         
         self.k = k
         
+    def distance(self, p1, p2): 
+        return np.sum((p1 - p2)**2) 
+        
     def initCentroids(self, k, data):
-        centroids = data.copy()
-        np.random.shuffle(centroids)
-        return centroids[:k]
+        centroids = []
+        centroids.append(data[np.random.randint( 
+            data.shape[0]), :])
+        if self.verbose:
+            print("Searching for best starting centroids...")
+            print("[1] Setting centroid to", centroids[0])
+        for c_id in range(k-1):
+            dist = [] 
+            for point in data: 
+                d = sys.maxsize 
+                for j in range(len(centroids)): 
+                    temp_dist = self.distance(point, centroids[j]) 
+                    d = min(d, temp_dist) 
+                dist.append(d) 
+                
+            dist = np.array(dist) 
+            next_centroid = data[np.argmax(dist), :] 
+            centroids.append(next_centroid)
+            if self.verbose:
+                print("[%d] Setting centroid to" % (c_id+2), centroids[c_id+1])
+            dist = [] 
+        centroids = np.array(centroids)
+        return centroids
     
     def calcDistances(self, centroids, data):
         distances = np.sqrt(((data - centroids[:, np.newaxis])**2).sum(axis=2))
         return distances
     
     def fit(self, data, max_iters=10, verbose=False, optimize=False):
+        self.verbose = verbose
         if optimize == False:
+            if self.verbose:
+                print("Finding the best starting centroids...")
             centroids = self.initCentroids(self.k, data)
 
             for epoch in range(max_iters):
+                if self.verbose:
+                    print("[%d] KMean'in..." % epoch)
                 distances = self.calcDistances(centroids, data)
                     
                 affiliations = np.argmin(distances, axis=0)
@@ -49,7 +78,7 @@ class kmeans:
                     centroids = np.array([data[affiliations==k].mean(axis=0) for k in range(centroids.shape[0])])
 
                 silscore = silhouette_score(data, affiliations)
-                if verbose:
+                if self.verbose:
                     print("[%d] Elbow'in... SilScore=%f" % (cK, silscore))
                 
                 if best[0] < silscore:
