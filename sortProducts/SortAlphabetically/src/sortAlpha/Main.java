c17f9b15995894d379898c847b4788561a639ca9
Davanchama <davanchama@gmail.com>
2020-09-28 12:18:30 +0200

    updated sort alghorithmn

--- sortProducts/SortAlphabetically/src/sortAlpha/Main.java
+++ sortProducts/SortAlphabetically/src/sortAlpha/Main.java
@@ -5,22 +5,23 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 public class Main {
 	static List<String> lines;
 
 	public static void main(String[] args) throws IOException {
-		try {
-			lines = Files.readAllLines(Paths.get("C:/Users/Prog/Desktop/GroceryListPredictor/webapp/allproducts2.txt"));
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		Collections.sort(lines);
-		System.out.println(lines);
+		//file input
+		lines = (Files.readAllLines(Paths.get("C:/Users/Prog/Desktop/GroceryListPredictor/webapp/allproducts2.txt")));
+		//sort and remove duplicates
+		Set<String> singlelines = new TreeSet<String>(lines);
+		//file output
 		FileWriter writer = new FileWriter("C:/Users/Prog/Desktop/GroceryListPredictor/webapp/sortedProducts.txt");
-		for (String str : lines) {
+		for (String str : singlelines) {
 			writer.write(str + System.lineSeparator());
 		}
 		writer.close();
